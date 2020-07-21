21a586300bec0e0cdaf9f5476e216c5050c5568c
Reza <ahmad.reza.musthafa2@gmail.com>
2020-07-21 20:56:57 +0700

    add core

---
+++ lib/config/api_config.dart
@@ -0,0 +1,5 @@
+class API {
+  static const MSEngineAPIUrl = 'http://www.thumbstalk.co';
+  static const MSEngineMQTTBrokerUrl = "messaging.thumbstalk.co";
+  static const MSEngineMQTTBrokerPort = 1883;
+}
